498f484678ae578c33e7e6aff70263878137b961
Dull Bananas <dull.bananas0@gmail.com>
2019-12-13 16:19:43 -0700

    add timing

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import time
+
+
 # Test Cython
 import pyximport; pyximport.install(language_level=3)
 import c_mod
+
+start_time = time.time()
 print(c_mod.primes(1000))
+elapsed = time.time() - start_time
+print(f'Cython took {elapsed} seconds to run')
