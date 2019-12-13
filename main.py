46c4e00c10fa26de79776573770ba5232cce8ff8
Dull Bananas <dull.bananas0@gmail.com>
2019-12-13 16:23:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ import pyximport; pyximport.install(language_level=3)
 import c_mod
 
 start_time = time.time()
-print(c_mod.primes(1000))
+c_mod.primes(1000)
 elapsed = time.time() - start_time
 print(f'Cython took {elapsed} seconds to run')
+
+
+# Test Nimporter
+import nimporter
+import nim_mod
+
+start_time = time.time()
+nim_mod.primes(1000)
+elapsed = time.time() - start_time
+print(f'Nimporter took {elapsed} seconds to run')
