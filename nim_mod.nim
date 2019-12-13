26e9c2cc33252afe9b2e94e76677753bf66a160b
Dull Bananas <dull.bananas0@gmail.com>
2019-12-13 16:46:39 -0700

    Create nim_mod.nim

---
+++ nim_mod.nim
@@ -0,0 +1,21 @@
+import nimpy
+
+proc primes() {.exportpy.} =
+    var n, i, len_p: int32
+    var p: array[0..1000, int32]
+    var uwu: bool
+    
+    len_p = 0
+    n = 2
+    while len_p < 1000:
+        uwu = true
+        for h in 0..len_p:
+            i = p[h]
+            if n % i == 0:
+                uwu = false
+                break
+        
+        if uwu:
+            p[len_p] = n
+            len_p += 1
+        n += 1
